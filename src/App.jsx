121395d0f5af8102a612375cfe4f8a20fd348c41
1xe1 <641463021@crru.ac.th>
2023-04-24 14:02:00 +0700

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect} from 'react'
 import logo from './assets/react.svg'
-export default class App extends Component {
-  state = {
-    count:0
+import axios from 'axios'
+
+
+export const App = () => {
+  const[count, setCount] =  useState(0)
+  const[users, setUsers] = useState([])
+
+  async function getUsers() {
+    try {
+      const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+      setUsers(data)
+    } catch (err) {
+      console.log(err)
+    }
   }
-  render() {
+
+  useEffect(() =>{
+    getUsers()
+    // axios.get('https://jsonplaceholder.typicode.com/users').then(res => setUsers(res.data)).catch((err) => console.log(err))
+
+  },[count])
+
     return (
       <div className="flex flex-col justify-center items-center gap-y-6 h-screen">
-        <img src={logo} className='w-40 cursor-pointer' alt='react logo' onClick={()=>this.setState({count:0})} />
-        <button type='button' className='block text-lg text-yellow-50 bg-[#00d8fd] p-3 rounded-lg hover:bg-black' onClick={()=>this.setState({ count: this.state.count +1})}>Count is: {this.state.count}</button>
+        <img src={logo} className='w-40 cursor-pointer' alt='react logo' onClick={()=>setCount(0)} />
+        <button type='button' className='block text-lg text-yellow-50 bg-[#00d8fd] p-3 rounded-lg hover:bg-black' onClick={()=>setCount(count+1)}>Count is: {count}</button>
+        <ul>
+          {users.map((user) => (<li key={user.id}>{user.name}</li>))}
+        </ul>
       </div>
     )
-  }
 }
+
+export default App
